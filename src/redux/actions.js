0586e13de24903e31182521880333c645ae14a53
JohnMoran <johnnymoran44@hotmail.com>
2021-05-20 12:49:47 +0200

    just using bearer as header in api call

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 export const fetchPosts = () => async (dispatch, getState) => {
-	const SEARCH_TWITTER_URL = "https://cors-john.herokuapp.com/https://api.twitter.com/2/tweets/search/recent";
+	//https://private-cors-server.herokuapp.com/
+	const SEARCH_TWITTER_URL = "https://cors-john.herokuapp.com/https://api.twitter.com/1.1/search/tweets.json";
 
 	const headers = {
-		Authorization: `OAuth oauth_consumer_key=${process.env.REACT_APP_API_KEY},oauth_token=${process.env.REACT_APP_ACCESS_TOKEN},oauth_signature_method="HMAC-SHA1",oauth_timestamp="1621440571",oauth_nonce="iJGPl3Z1gZC",oauth_version="1.0",oauth_signature="ALQc4K%2FhPplpZEGXIaCwu2wEFWI%3D"`,
+		Authorization: `Bearer ${process.env.REACT_APP_TWITTER_BEARER_TOKEN}`,
 	};
 
 	dispatch({
@@ -12,12 +13,12 @@ export const fetchPosts = () => async (dispatch, getState) => {
 	});
 
 	try {
-		const response = await axios.get(`${SEARCH_TWITTER_URL}?query=arsenal`, { headers: headers });
-		console.log(response.data.data);
+		const response = await axios.get(`${SEARCH_TWITTER_URL}?q=%23hulk&lang=es`, { headers: headers });
+		console.log(response);
 
 		dispatch({
 			type: "FETCH_TWEETS_SUCCESS",
-			payload: response.data.data,
+			payload: response.data.statuses,
 		});
 	} catch (error) {
 		dispatch({
