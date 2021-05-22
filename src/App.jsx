1edd5b7ad6d50fdac81f6e79bc4e99d2a133c510
JohnMoran <johnnymoran44@hotmail.com>
2021-05-22 15:20:20 +0200

    navbar and react router, about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import Posts from "./components/Posts";
 import styled from "styled-components";
+
+import About from "./components/About";
 
 const App = () => {
 	return (
 		<Container>
-			<Posts />
+			<BrowserRouter>
+				<NavBar>
+					<Link to="/">Home </Link>|<Link to="/about"> About</Link>
+				</NavBar>
+				<Switch>
+					<Route exact path="/" component={Posts}></Route>
+					<Route exact path="/about" component={About} />
+				</Switch>
+			</BrowserRouter>
 		</Container>
 	);
 };
@@ -16,4 +27,12 @@ export default App;
 const Container = styled.div`
 	max-width: 600px;
 	margin: 0 auto;
+`;
+
+const NavBar = styled.nav`
+	color: white;
+	> a {
+		color: white;
+		text-decoration: none;
+	}
 `;
